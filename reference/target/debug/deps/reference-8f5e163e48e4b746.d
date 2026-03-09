f67457ad6841cc96a0d22450669e100c9f28a4b3
GermanViter <german.viter@icloud.com>
2026-03-08 22:54:13 -0400

    chore: stop tracking reference/target directory

--- reference/target/debug/deps/reference-8f5e163e48e4b746.d
+++
@@ -1,5 +0,0 @@
-/Users/germaviter/Desktop/portfolio/rust_projects/rust_learning/reference/target/debug/deps/reference-8f5e163e48e4b746.d: src/main.rs
-
-/Users/germaviter/Desktop/portfolio/rust_projects/rust_learning/reference/target/debug/deps/libreference-8f5e163e48e4b746.rmeta: src/main.rs
-
-src/main.rs:
--- reference/target/debug/reference.d
+++
@@ -1,1 +0,0 @@
-/Users/germaviter/Desktop/portfolio/rust_projects/rust_learning/reference/target/debug/reference: /Users/germaviter/Desktop/portfolio/rust_projects/rust_learning/reference/src/main.rs
